e1df764382e4b3f2a911c9b812cfe2e799494da2
Thibault - "Aldrin <77836986+t3hyx@users.noreply.github.com>
2025-04-23 17:57:45 +0200

    feat: reviewed solarSystemConfig accordingly to new type structure

--- src/configs/solar-system.config.ts
+++ src/configs/solar-system.config.ts
@@ -1,15 +1,46 @@
-import type { ISolarSystemConfig } from '@/types/solar-system.types'
+import type { ICelestialBody, ISolarSystemConfig } from '@/types/solar-system.types'
 import { colorConfig } from '@/configs/color.config'
 import * as THREE from 'three'
 
 export const solarSystemConfig: ISolarSystemConfig = {
-  fov: 75,
-  near: 0.1,
-  far: 1000,
-  cameraPosition: new THREE.Vector3(0, 0, 10),
-  backgroundColor: colorConfig.pureBlack,
-  lightColor: colorConfig.pureWhite,
-  lightIntensity: 1,
-  ambientLightColor: colorConfig.ambientGray,
-  ambientLightIntensity: 0.5,
+  camera: {
+    fov: 75,
+    near: 0.1,
+    far: 1000,
+    position: new THREE.Vector3(0, 0, 50),
+  },
+  background: {
+    color: colorConfig.pureBlack,
+  },
+  lights: {
+    sun: {
+      color: colorConfig.pureWhite,
+      intensity: 1.00,
+      distance: 1000,
+      decay: 2.00,
+    },
+    ambient: {
+      color: colorConfig.ambientGray,
+      intensity: 0.5,
+    },
+  },
+  stars: {
+    color: colorConfig.pureWhite,
+    count: 5000,
+    size: 0.5,
+    sizeAttenuation: true,
+  },
+}
+
+export const celestialBodiesConfig: Record<string, ICelestialBody> = {
+  sun: {
+    name: 'Sun',
+    radius: 1,
+    distance: 0,
+    color: colorConfig.sunYellow,
+    emissive: colorConfig.sunraysOrange,
+    scale: 5,
+    segments: 64,
+    shininess: 100,
+  },
 }

7b24bbdca8b389d04a1b2cace5b6be20e7625527
Thibault - "Aldrin <77836986+t3hyx@users.noreply.github.com>
2025-04-24 16:18:50 +0200

    feat: updated AxisGridsHelper to grants visibility-toggling capacity to axes & grids

--- src/utils/AxisGridsHelper.ts
+++ src/utils/AxisGridsHelper.ts
@@ -3,11 +3,12 @@ import * as THREE from 'three'
 export class AxisGridsHelper {
   private axes: THREE.AxesHelper
   private grid: THREE.GridHelper
+  private _visible: boolean = false
 
   constructor(node: THREE.Object3D, units: number = 10) {
     this.axes = new THREE.AxesHelper()
     this.axes.renderOrder = 2 // render after grids
-    this.axes.visible = true
+    this.axes.visible = false
 
     if (this.axes.material instanceof THREE.Material) {
       this.axes.material.depthTest = false
@@ -17,12 +18,22 @@ export class AxisGridsHelper {
 
     this.grid = new THREE.GridHelper(units, units)
     this.grid.renderOrder = 1 // render after other objects but before axes
-    this.grid.visible = true
+    this.grid.visible = false
 
     if (this.grid.material instanceof THREE.Material) {
       this.grid.material.depthTest = false
     }
     // add grid to the node
     node.add(this.grid)
+  }
+
+  get visible() {
+    return this._visible
+  }
+
+  set visible(value: boolean) {
+    this._visible = value
+    this.grid.visible = value
+    this.axes.visible = value
   }
 }

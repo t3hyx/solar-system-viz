89bdcee7971132756b6f8ac4cc7bd6b33bd239a9
Thibault - "Aldrin <77836986+t3hyx@users.noreply.github.com>
2025-04-28 16:37:23 +0200

    feat: updated CelestialBodyFactory to use an other property (orbitalRotationSpeed) and added a togglable visual representation of orbits

--- src/factories/CelestialBodyFactory.ts
+++ src/factories/CelestialBodyFactory.ts
@@ -12,17 +12,34 @@ export class CelestialBodyFactory {
       color: body.color,
       emissive: body.emissive,
       shininess: body.shininess,
+      wireframe: body.wireframed,
     })
     const mesh = new THREE.Mesh(geometry, material)
     mesh.scale.set(body.scale, body.scale, body.scale)
+    mesh.name = body.name
 
     return mesh
   }
 
   // * Creates an orbit object for a celestial body
-  public static createOrbit(distance: number): THREE.Object3D {
+  public static createOrbit(distance: number, bodyName: string): THREE.Object3D {
     const orbit = new THREE.Object3D()
-    orbit.position.x = distance
+    orbit.name = `orbit-${bodyName}`
+
+    // Create a visual representation of the orbit
+    const orbitGeometry = new THREE.RingGeometry(distance - 0.1, distance + 0.1, 64)
+    const orbitMaterial = new THREE.MeshBasicMaterial({
+      color: 0x444444,
+      side: THREE.DoubleSide,
+      transparent: true,
+      opacity: 0.3,
+    })
+    const orbitMesh = new THREE.Mesh(orbitGeometry, orbitMaterial)
+    orbit.add(orbitMesh)
+
+    // Store the orbit mesh as a property for easy access
+    orbit.userData.orbitMesh = orbitMesh
+
     return orbit
   }
 
@@ -39,7 +56,8 @@ export class CelestialBodyFactory {
       segments: this.config.sun.segments,
       shininess: this.config.sun.shininess,
       selfRotationSpeed: this.config.sun.selfRotationSpeed,
-      wireframe: this.config.sun.wireframe,
+      orbitalRotationSpeed: this.config.sun.orbitalRotationSpeed,
+      wireframed: this.config.sun.wireframed,
     })
   }
 
@@ -56,7 +74,8 @@ export class CelestialBodyFactory {
       segments: this.config.earth.segments,
       shininess: this.config.earth.shininess,
       selfRotationSpeed: this.config.earth.selfRotationSpeed,
-      wireframe: this.config.earth.wireframe,
+      orbitalRotationSpeed: this.config.earth.orbitalRotationSpeed,
+      wireframed: this.config.earth.wireframed,
     })
   }
 
@@ -73,7 +92,8 @@ export class CelestialBodyFactory {
       segments: this.config.moon.segments,
       shininess: this.config.moon.shininess,
       selfRotationSpeed: this.config.moon.selfRotationSpeed,
-      wireframe: this.config.moon.wireframe,
+      orbitalRotationSpeed: this.config.moon.orbitalRotationSpeed,
+      wireframed: this.config.moon.wireframed,
     })
   }
 
@@ -81,16 +101,27 @@ export class CelestialBodyFactory {
   public static createSaturnWithRings(): THREE.Group {
     // Create Saturn group
     const saturnGroup = new THREE.Group()
+    saturnGroup.name = this.config.saturn.name
 
     // Create Saturn planet
-    const saturnGeometry = new THREE.SphereGeometry(1, 32, 32)
+    const saturnGeometry = new THREE.SphereGeometry(
+      this.config.saturn.radius,
+      this.config.saturn.segments,
+      this.config.saturn.segments,
+    )
     const saturnMaterial = new THREE.MeshPhongMaterial({
       color: this.config.saturn.color,
       emissive: this.config.saturn.emissive,
+      emissiveIntensity: this.config.saturn.emissiveIntensity,
       shininess: this.config.saturn.shininess,
+      wireframe: this.config.saturn.wireframed,
     })
     const saturn = new THREE.Mesh(saturnGeometry, saturnMaterial)
-    saturn.scale.set(2.5, 2.5, 2.5)
+    saturn.scale.set(
+      this.config.saturn.scale,
+      this.config.saturn.scale,
+      this.config.saturn.scale,
+    )
     saturnGroup.add(saturn)
 
     // Create Saturn rings
@@ -101,6 +132,7 @@ export class CelestialBodyFactory {
       transparent: true,
       opacity: 0.8,
       shininess: 30,
+      wireframe: this.config.saturn.wireframed,
     })
     const rings = new THREE.Mesh(ringGeometry, ringMaterial)
     rings.rotation.x = Math.PI / 2 // x Rotate to be horizontal

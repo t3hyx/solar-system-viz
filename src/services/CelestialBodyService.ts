ae3e38e8cae2e75751742db7b5004604033fcb01
Thibault - "Aldrin <77836986+t3hyx@users.noreply.github.com>
2025-05-05 20:58:47 +0200

    feat: refactor CelestialBodyService to utilize CelestialBodyFactory for planet and moon creation, enhancing modularity and async handling

--- src/services/CelestialBodyService.ts
+++ src/services/CelestialBodyService.ts
@@ -1,6 +1,6 @@
+import type { CelestialBodyFactory } from '@/factories/CelestialBodyFactory'
 import type { ISolarSystemState } from '@/types/solar-system.types'
 import { celestialBodiesConfig } from '@/configs/celestial-bodies.config'
-import { CelestialBodyFactory } from '@/factories/CelestialBodyFactory'
 import * as THREE from 'three'
 
 /**
@@ -14,109 +14,63 @@ import * as THREE from 'three'
  */
 export class CelestialBodyService {
   private state: ISolarSystemState
+  private celestialBodyFactory: CelestialBodyFactory
 
-  constructor(state: ISolarSystemState) {
+  constructor(state: ISolarSystemState, celestialBodyFactory: CelestialBodyFactory) {
     this.state = state
+    this.celestialBodyFactory = celestialBodyFactory
   }
 
-  public createSolarSystem(): void {
+  public async createSolarSystem(): Promise<void> {
     // create solar system container
     const solarSystem = new THREE.Object3D()
     this.state.scene.add(solarSystem)
     this.state.objects.push(solarSystem)
 
     // create Sun
-    const sun = CelestialBodyFactory.createSun()
+    const sun = await this.celestialBodyFactory.createPlanet('sun')
     solarSystem.add(sun)
     this.state.objects.push(sun)
 
     // create planets
-    this.createPlanet('mercury', solarSystem)
-    this.createPlanet('venus', solarSystem)
-    this.createPlanet('earth', solarSystem)
-    this.createPlanet('mars', solarSystem)
-    this.createPlanet('jupiter', solarSystem)
-    this.createPlanet('saturn', solarSystem)
-    this.createPlanet('uranus', solarSystem)
-    this.createPlanet('neptune', solarSystem)
-    this.createPlanet('pluto', solarSystem)
+    const planets = [
+      'mercury',
+      'venus',
+      'earth',
+      'mars',
+      'jupiter',
+      'saturn',
+      'uranus',
+      'neptune',
+      'pluto',
+    ]
 
-    // create Moon (special case as it orbits Earth)
-    this.createMoon()
-  }
+    for (const planetName of planets) {
+      const planet = await this.celestialBodyFactory.createPlanet(planetName)
+      const config = celestialBodiesConfig[planetName]
 
-  private createPlanet(planetKey: keyof typeof celestialBodiesConfig, parent: THREE.Object3D): void {
-    const config = celestialBodiesConfig[planetKey]
+      // Set planet position
+      planet.position.x = config.distance
 
-    // create orbit
-    const orbit = CelestialBodyFactory.createOrbit(config.distance, config.name)
-    parent.add(orbit)
-    this.state.objects.push(orbit)
+      // Create orbit and trail
+      const orbit = this.celestialBodyFactory.createOrbit(config.distance, planetName)
+      const trail = this.celestialBodyFactory.createOrbitTrail(config.distance, planetName)
 
-    // create trail
-    const trail = CelestialBodyFactory.createOrbitTrail(config.distance, config.name)
-    parent.add(trail)
-    this.state.objects.push(trail)
+      solarSystem.add(orbit)
+      solarSystem.add(trail)
+      orbit.add(planet)
 
-    // create planet
-    let planet: THREE.Object3D
-    switch (planetKey) {
-      case 'mercury':
-        planet = CelestialBodyFactory.createMercury()
-        break
-      case 'venus':
-        planet = CelestialBodyFactory.createVenus()
-        break
-      case 'earth':
-        planet = CelestialBodyFactory.createEarth()
-        break
-      case 'mars':
-        planet = CelestialBodyFactory.createMars()
-        break
-      case 'jupiter':
-        planet = CelestialBodyFactory.createJupiter()
-        break
-      case 'saturn':
-        planet = CelestialBodyFactory.createSaturnWithRings()
-        break
-      case 'uranus':
-        planet = CelestialBodyFactory.createUranus()
-        break
-      case 'neptune':
-        planet = CelestialBodyFactory.createNeptune()
-        break
-      case 'pluto':
-        planet = CelestialBodyFactory.createPluto()
-        break
-      default:
-        throw new Error(`Unknown planet: ${planetKey}`)
+      this.state.objects.push(orbit, trail, planet)
     }
 
-    planet.position.x = config.distance
-    orbit.add(planet)
-    this.state.objects.push(planet)
-  }
-
-  private createMoon(): void {
-    const earth = this.state.objects.find(obj => obj.name === 'Earth')
-    if (!earth)
-      return
-
-    const config = celestialBodiesConfig.moon
-
-    // create orbit
-    const orbit = CelestialBodyFactory.createOrbit(config.distance, config.name)
-    earth.add(orbit)
-
-    // create trail
-    const trail = CelestialBodyFactory.createOrbitTrail(config.distance, config.name)
-    earth.add(trail)
-    this.state.objects.push(trail)
-
-    // create moon
-    const moon = CelestialBodyFactory.createMoon()
-    moon.position.x = config.distance
-    orbit.add(moon)
-    this.state.objects.push(moon)
+    // create Moon (special case as it orbits Earth)
+    const moon = await this.celestialBodyFactory.createPlanet('moon')
+    const earth = this.state.objects.find(obj => obj.name === 'earth')
+    if (earth) {
+      const moonConfig = celestialBodiesConfig.moon
+      moon.position.x = moonConfig.distance
+      earth.add(moon)
+      this.state.objects.push(moon)
+    }
   }
 }
